4b8c50dea77c906c86c9582004b7b1eeb9766011
DiegoBP12 <diegobolpar@hotmail.com>
2020-06-22 10:43:09 -0500

    Se generan las definiciones en el archivo views.py

--- web/views.py
+++ web/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request, "sitio_web/home.html")
+
+def acceso(request):
+    return render(request, "sitio_web/acceso.html")
+
+def perfil_admin(request):
+    return render(request, "sitio_web/perfil_admin.html")
+
+def perfil_vendedor(request):
+    return render(request, "sitio_web/perfil_vendedor.html")
+
+def perfil_consumidor(request):
+    return render(request, "sitio_web/perfil_consumidor.html")
