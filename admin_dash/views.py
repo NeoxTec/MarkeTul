710df9146396bd377dcd346c2a8c087876fdc0d2
DiegoBP12 <diegobolpar@hotmail.com>
2020-07-02 02:58:12 -0500

    se añaden las definiciones de views.py de web a views.py de admin_dash

--- admin_dash/views.py
+++ admin_dash/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+""" Dashboard Administrador """
+
+def admin_dash(request):
+    return render(request, "admin_dash/admin_dash.html")
+
+def vendedores(request):
+    return render(request, "admin_dash/vendedores.html")
+
+def admin_productos(request):
+    return render(request, "admin_dash/admin_productos.html") 
+
+def admin_detalle_producto(request):
+    return render(request, "admin_dash/admin_detalle_producto.html")
+
+def admin_nuevo_producto(request):
+    return render(request, "admin_dash/admin_nuevo_producto.html") 
+
+def admin_solicitudes_vendedor(request):
+    return render(request, "admin_dash/admin_solicitudes_vendedor.html") 
+
+def admin_detalle_solicitud(request):
+    return render(request, "admin_dash/admin_detalle_solicitud.html") 
+
+def admin_rechazo_solicitud(request):
+    return render(request, "admin_dash/admin_rechazo_solicitud.html") 
+
+def config_admin(request):
+    return render(request, "admin_dash/config_admin.html") 
--- web/views.py
+++ web/views.py
@@ -40,34 +40,7 @@ def perfil_vendedor(request):
 def perfil_consumidor(request):
     return render(request, "sitio_web/perfil_consumidor.html")
 
-""" Dashboard Administrador """
 
-def admin_dash(request):
-    return render(request, "sitio_web/admin_dash.html")
-
-def vendedores(request):
-    return render(request, "sitio_web/vendedores.html")
-
-def admin_productos(request):
-    return render(request, "sitio_web/admin_productos.html") 
-
-def admin_detalle_producto(request):
-    return render(request, "sitio_web/admin_detalle_producto.html")
-
-def admin_nuevo_producto(request):
-    return render(request, "sitio_web/admin_nuevo_producto.html") 
-
-def admin_solicitudes_vendedor(request):
-    return render(request, "sitio_web/admin_solicitudes_vendedor.html") 
-
-def admin_detalle_solicitud(request):
-    return render(request, "sitio_web/admin_detalle_solicitud.html") 
-
-def admin_rechazo_solicitud(request):
-    return render(request, "sitio_web/admin_rechazo_solicitud.html") 
-
-def config_admin(request):
-    return render(request, "sitio_web/config_admin.html") 
 
 """ Dashboard Vendedor"""
 
