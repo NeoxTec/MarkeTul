1391717d39c037ac58e846390de11c672b107a1c
DiegoBP12 <diegobolpar@hotmail.com>
2020-07-02 03:24:10 -0500

    se corrige views.py de tienda

--- tienda/views.py
+++ tienda/views.py
@@ -5,37 +5,37 @@ from django.shortcuts import render
 """Tienda"""
 
 def categorias(request):
-    return render(request, "sitio_web/categorias.html")
+    return render(request, "tienda/categorias.html")
 
 def compras(request):
-    return render(request, "sitio_web/compras.html")
+    return render(request, "tienda/compras.html")
 
 def carrito_compras(request):
-    return render(request, "sitio_web/carrito_compras.html")
+    return render(request, "tienda/carrito_compras.html")
 
 def detalle_producto(request):
-    return render(request, "sitio_web/detalle_producto.html")
+    return render(request, "tienda/detalle_producto.html")
 
 def configuracion_cuenta(request):
-    return render(request, "sitio_web/configuracion_cuenta.html")
+    return render(request, "tienda/configuracion_cuenta.html")
 
 def categoria_computo(request):
-    return render(request, "sitio_web/categoria_computo.html")
+    return render(request, "tienda/categoria_computo.html")
 
 def direccion_envio(request):
-    return render(request, "sitio_web/direccion_envio.html")
+    return render(request, "tienda/direccion_envio.html")
 
 def forma_pago(request):
-    return render(request, "sitio_web/forma_pago.html")
+    return render(request, "tienda/forma_pago.html")
 
 def proceso_pago(request):
-    return render(request, "sitio_web/proceso_pago.html")
+    return render(request, "tienda/proceso_pago.html")
 
 def pago_error(request):
-    return render(request, "sitio_web/pago_error.html")
+    return render(request, "tienda/pago_error.html")
 
 def pago_exitoso(request):
-    return render(request, "sitio_web/pago_exitoso.html")
+    return render(request, "tienda/pago_exitoso.html")
 
 def catalogos(request):
-    return render(request, "sitio_web/catalogos.html")
+    return render(request, "tienda/catalogos.html")
