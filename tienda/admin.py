776ccee23c0fffe6273570a7d78a207d92a0658c
Jireh Hernandez Castillo <1717110620@utectulancingo.edu.mx>
2020-07-03 09:54:59 -0500

    creacion de modelos Compra y Forma de pago

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from tienda.models import Compras , Forma_Pago
 # Register your models here.
+
+admin.site.register(Compras)
+admin.site.register(Forma_Pago)
--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Compras(models.Model):
+    idCompra = models.BigIntegerField(primary_key=True, auto_created=True)
+    producto = models.CharField(max_length=100)
+    tienda = models.CharField(max_length=50)
+    cantidad = models.IntegerField()   
+
+class Forma_Pago(models.Model):
+    idForma_pago = models.BigIntegerField(primary_key=True, auto_created=True)
+    nombre_propietario = models.CharField(max_length=50)
+    numero_tarjeta = models.IntegerField()
+    vencimiento_dia = models.DateField()
+    vencimiento_mes = models.DateField()
+    vencimiento_anio = models.DateField()
