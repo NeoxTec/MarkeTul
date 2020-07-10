d9fa78259e828908cb004c6b043bbf23ea01e279
DiegoBP12 <diegobolpar@hotmail.com>
2020-07-09 23:16:15 -0500

    Se añaden cambios en los templates

--- vendedor/models.py
+++ vendedor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from admin_dash.models import Tienda
+from django.contrib.auth.models import User
 
 # Create your models here.
+class SolicitudesVendedor(models.Model):
+    idSolVen = models.BigIntegerField(primary_key=True,auto_created=True)
+    motivos = models.TextField()
+    status = models.SmallIntegerField()
+    idTi = models.ForeignKey(Tienda, null=True, blank=True, on_delete=models.CASCADE)
+    idVen = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
