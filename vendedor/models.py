387eae99afb4a0aab15d08e35f78fe202af4e18e
DiegoBP12 <diegobolpar@hotmail.com>
2020-07-10 00:26:14 -0500

    Se añaden modelo de vendedor

--- vendedor/models.py
+++ vendedor/models.py
@@ -6,13 +6,14 @@ from django.contrib.auth.models import User
 class Vendedor(models.Model):
     idVend = models.BigIntegerField(primary_key=True, auto_created=True)
     nombreVend = models.CharField(max_length=200)
-    edadVen = models.IntegerField()
     telefono = models.IntegerField()
     idUser = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
 
 class SolicitudesVendedor(models.Model):
     idSolVen = models.BigIntegerField(primary_key=True,auto_created=True)
     motivos = models.TextField()
+    edadVen = models.IntegerField(null=True,blank=True)
+    genero = models.CharField(max_length=50, null=True,blank=True)
     status = models.SmallIntegerField()
     idTi = models.ForeignKey(Tienda, null=True, blank=True, on_delete=models.CASCADE)
     idVen = models.ForeignKey(Vendedor, null=True, blank=True, on_delete=models.CASCADE)
