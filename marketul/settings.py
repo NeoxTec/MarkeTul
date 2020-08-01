cb9119c6d101bbc7f7967e86bd8c63c41952e16d
DiegoBP12 <diegobolpar@hotmail.com>
2020-07-31 20:25:35 -0500

    cambio en settings

--- marketul/settings.py
+++ marketul/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'),)
 
 
 # Media Files
